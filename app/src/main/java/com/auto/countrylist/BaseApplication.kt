dbfdaf45c0f018c35f680d1fb22403b681dea54e
Chrisroid <chrisroiddev@gmail.com>
2022-11-12 07:42:01 +0100

    module file for dependencies created

--- app/src/main/java/com/auto/countrylist/BaseApplication.kt
+++ app/src/main/java/com/auto/countrylist/BaseApplication.kt
@@ -1,4 +1,7 @@
 package com.auto.countrylist
 
-class BaseApplication {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application()
--- app/src/main/java/com/auto/countrylist/di/AppModule.kt
+++ app/src/main/java/com/auto/countrylist/di/AppModule.kt
@@ -1,4 +1,34 @@
 package com.auto.countrylist.di
 
-class AppModule {
+import com.auto.countrylist.common.Constants
+import com.auto.countrylist.data.remote.CountriesApi
+import com.auto.countrylist.data.repository.CountriesRepositoryImpl
+import com.auto.countrylist.domain.repository.CountriesRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule
+{
+    @Provides
+    @Singleton
+    fun provideCountriesApi(): CountriesApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CountriesApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCountriesRepository(api: CountriesApi): CountriesRepository {
+        return CountriesRepositoryImpl(api)
+    }
 }
